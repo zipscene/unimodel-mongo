20b70f1b607fd2c971b7592dac3f44659d879964
Chris Breneman <crispy@cluenet.org>
2015-07-31 15:09:02 -0400

    More

--- test/unimongo-model.js
+++ test/unimongo-model.js
@@ -15,6 +15,15 @@ describe('UnimongoModel', function() {
 		return model.collectionPromise;
 	});
 
+	it('should not fail if the collection already exists', function() {
+		let model1 = createModel('testings', { foo: String });
+		return model1.collectionPromise
+			.then(() => {
+				let model2 = createModel('testings', { foo: String });
+				return model2.collectionPromise;
+			});
+	});
+
 });
 
 
