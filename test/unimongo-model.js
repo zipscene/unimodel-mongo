3f85eca82c1bce1f975c61f032e8a345fc5bce26
Chris Breneman <crispy@cluenet.org>
2015-08-06 10:05:42 -0400

    More index and collection handling

--- test/unimongo-model.js
+++ test/unimongo-model.js
@@ -24,6 +24,31 @@ describe('UnimongoModel', function() {
 			});
 	});
 
+	it('should construct the appropriate indices', function() {
+		let model = createModel('testings', {
+			foo: { type: String, index: true },
+			bar: { type: Number, unique: true },
+			baz: {
+				buz: [ { type: 'geopoint', index: true } ],
+				bip: { type: 'geojson', index: '2dsphere' },
+				zap: { type: String, index: true, sparse: true },
+				zip: { type: Number, index: -1 }
+			}
+		}, {
+			initialize: false
+		});
+		model.index({ foo: 1, bar: -1, 'baz.buz': '2dsphere' }, { unique: true, sparse: true });
+		expect(model.getIndices()).to.deep.equal([
+			{ spec: { foo: 1 }, options: {} },
+			{ spec: { bar: 1 }, options: { unique: true } },
+			{ spec: { 'baz.buz': '2dsphere' }, options: {} },
+			{ spec: { 'baz.bip': '2dsphere' }, options: {} },
+			{ spec: { 'baz.zap': 1 }, options: { sparse: true } },
+			{ spec: { 'baz.zip': -1 }, options: {} },
+			{ spec: { foo: 1, bar: -1, 'baz.buz': '2dsphere' }, options: { unique: true, sparse: true } }
+		]);
+	});
+
 });
 
 
