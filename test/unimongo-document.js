91752a95525195bbf9cd4b0126d3ab419b11f241
Ross Hadden <rosshadden@gmail.com>
2015-08-28 10:43:16 -0400

    Split document tests into their own file.

---
+++ test/unimongo-document.js
@@ -0,0 +1,166 @@
+const chai = require('chai');
+const XError = require('xerror');
+const expect = chai.expect;
+const {
+	createModel,
+	UnimongoDb,
+	UnimongoError,
+	UnimongoModel,
+	UnimongoDocument
+} = require('../lib');
+const pasync = require('pasync');
+const testScaffold = require('./lib/mongo-scaffold');
+
+chai.use(require('chai-as-promised'));
+
+describe('UnimongoDocument', function() {
+
+	beforeEach(testScaffold.resetAndConnect);
+
+	it('should move internal id to the instance', function() {
+		let model = createModel('testings', { foo: Number });
+
+		return model
+			.insert({ foo: '123' })
+			.then((result) => {
+				expect(result._id).to.exist;
+				expect(result._originalId).to.exist;
+				expect(result.getInternalId()).to.exist;
+				expect(result.data._id).to.be.undefined;
+				expect(result._originalData._id).to.be.undefined;
+			});
+	});
+
+	it('should save changes to a new document', function() {
+		let model = createModel('testings', { foo: String });
+
+		let document = new UnimongoDocument(model, {
+			foo: 'bar'
+		});
+
+		document.data.foo = 'baz';
+
+		return document.save()
+			.then((document) => {
+				expect(document.data).to.deep.equal({ foo: 'baz' });
+			});
+	});
+
+	it('should save changes to an existing document', function() {
+		let model = createModel('testings', { foo: String });
+
+		let document = new UnimongoDocument(model, {
+			foo: 'bar'
+		});
+
+		return document.save()
+			.then((document) => {
+				document.data.foo = 'baz';
+
+				return document.save();
+			})
+			.then((document) => {
+				expect(document.data).to.deep.equal({ foo: 'baz' });
+			});
+	});
+
+	it('should save changes to an existing document with a changed id', function() {
+		let model = createModel('testings', { foo: String });
+
+		let document = new UnimongoDocument(model, { foo: 'bar' });
+
+		return document.save()
+			.then((document) => {
+				document.setInternalId('some-other-id');
+				document.data.foo = 'baz';
+
+				return document.save();
+			})
+			.then((document) => {
+				expect(document.getInternalId()).to.equal('some-other-id');
+			});
+	});
+
+	it('should error when updating document id to an existing one', function() {
+		let model = createModel('testings', { foo: String });
+
+		let document = new UnimongoDocument(model, { foo: 'bar' });
+		let document2 = new UnimongoDocument(model, { foo: 'baz' });
+
+		let existingId;
+		return document.save()
+			.then((document) => {
+				existingId = document.getInternalId();
+				return document2.save();
+			})
+			.then((document2) => {
+				document2.setInternalId(existingId);
+
+				expect(document2.save()).to.be.rejectedWith(XError);
+			});
+	});
+
+	it('should save changes to existing data with no document', function() {
+		let model = createModel('testings', { foo: String });
+
+		return model.insert({ foo: 'bar' })
+			.then((document) => {
+				document.data.foo = 'baz';
+				return document.save();
+			})
+			.then((document) => {
+				expect(document.data).to.deep.equal({ foo: 'baz' });
+			});
+	});
+
+	it('should remove documents with UnimongoDocument#remove', function() {
+		let model = createModel('testings', { foo: String });
+
+		return model.insert({ foo: 'bar' })
+			.then((document) => document.remove())
+			.then((document) => model.find({ foo: 'bar' }))
+			.then((result) => {
+				expect(result).to.be.empty;
+			});
+	});
+
+	it('should properly handle saving partial documents in UnimongoDocument#save', function() {
+		let model = createModel('testings', { foo: Number, bar: Number });
+		let model2 = createModel('testings', { foo: Number, bar: Number }, { allowSavingPartials: false });
+
+		return model.insert({ foo: 1, bar: 1 })
+			.then(() => model.find({ foo: 1 }, { fields: [ 'bar' ] }))
+			.then((documents) => {
+				let document = documents[0];
+				document.data.foo = 2;
+				expect(() => document.save()).to.not.throw(Error);
+			})
+			.then(() => model2.find({ foo: 1 }, { fields: [ 'bar' ] }))
+			.then((documents) => {
+				let document = documents[0];
+				document.data.foo = 2;
+				expect(() => document.save()).to.throw(Error);
+			});
+	});
+
+	it('should handle setting and removing items from the same array in UnimongoDocument#save', function() {
+		let model = createModel('testings', { foo: [ String ] });
+
+		let document = new UnimongoDocument(model, {
+			foo: [ 'a', 'b', 'c' ]
+		});
+
+		return document.save()
+			.then((document) => {
+				document.data.foo = [ 'a', 'c' ];
+
+				return document.save();
+			})
+			.then((document) => {
+				expect(document.data.foo.length).to.equal(2);
+				expect(document.data.foo[0]).to.equal('a');
+				expect(document.data.foo[1]).to.equal('c');
+			});
+	});
+
+});
--- test/unimongo-model.js
+++ test/unimongo-model.js
@@ -185,89 +185,6 @@ describe('UnimongoModel', function() {
 			});
 	});
 
-	it('should move internal id to the instance', function() {
-		let model = createModel('testings', { foo: Number });
-
-		return model
-			.insert({ foo: '123' })
-			.then((result) => {
-				expect(result._id).to.exist;
-				expect(result._originalId).to.exist;
-				expect(result.getInternalId()).to.exist;
-				expect(result.data._id).to.be.undefined;
-				expect(result._originalData._id).to.be.undefined;
-			});
-	});
-
-	it('should save changes to a new document', function() {
-		let model = createModel('testings', { foo: String });
-
-		let document = new UnimongoDocument(model, {
-			foo: 'bar'
-		});
-
-		document.data.foo = 'baz';
-
-		return document.save()
-			.then((document) => {
-				expect(document.data).to.deep.equal({ foo: 'baz' });
-			});
-	});
-
-	it('should save changes to an existing document', function() {
-		let model = createModel('testings', { foo: String });
-
-		let document = new UnimongoDocument(model, {
-			foo: 'bar'
-		});
-
-		return document.save()
-			.then((document) => {
-				document.data.foo = 'baz';
-
-				return document.save();
-			})
-			.then((document) => {
-				expect(document.data).to.deep.equal({ foo: 'baz' });
-			});
-	});
-
-	it('should save changes to an existing document with a changed id', function() {
-		let model = createModel('testings', { foo: String });
-
-		let document = new UnimongoDocument(model, { foo: 'bar' });
-
-		return document.save()
-			.then((document) => {
-				document.setInternalId('some-other-id');
-				document.data.foo = 'baz';
-
-				return document.save();
-			})
-			.then((document) => {
-				expect(document.getInternalId()).to.equal('some-other-id');
-			});
-	});
-
-	it('should error when updating document id to an existing one', function() {
-		let model = createModel('testings', { foo: String });
-
-		let document = new UnimongoDocument(model, { foo: 'bar' });
-		let document2 = new UnimongoDocument(model, { foo: 'baz' });
-
-		let existingId;
-		return document.save()
-			.then((document) => {
-				existingId = document.getInternalId();
-				return document2.save();
-			})
-			.then((document2) => {
-				document2.setInternalId(existingId);
-
-				expect(document2.save()).to.be.rejectedWith(XError);
-			});
-	});
-
 	it('should update documents with UnimongoModel#update', function() {
 		let model = createModel('testings', { foo: String });
 
@@ -280,30 +197,6 @@ describe('UnimongoModel', function() {
 			.then((documents) => {
 				expect(documents.length).to.equal(1);
 				expect(documents[0].data.foo).to.equal('baz');
-			});
-	});
-
-	it('should save changes to existing data with no document', function() {
-		let model = createModel('testings', { foo: String });
-
-		return model.insert({ foo: 'bar' })
-			.then((document) => {
-				document.data.foo = 'baz';
-				return document.save();
-			})
-			.then((document) => {
-				expect(document.data).to.deep.equal({ foo: 'baz' });
-			});
-	});
-
-	it('should remove documents with UnimongoDocument#remove', function() {
-		let model = createModel('testings', { foo: String });
-
-		return model.insert({ foo: 'bar' })
-			.then((document) => document.remove())
-			.then((document) => model.find({ foo: 'bar' }))
-			.then((result) => {
-				expect(result).to.be.empty;
 			});
 	});
 
@@ -353,25 +246,6 @@ describe('UnimongoModel', function() {
 			});
 	});
 
-	it('should properly handle saving partial documents in UnimongoDocument#save', function() {
-		let model = createModel('testings', { foo: Number, bar: Number });
-		let model2 = createModel('testings', { foo: Number, bar: Number }, { allowSavingPartials: false });
-
-		return model.insert({ foo: 1, bar: 1 })
-			.then(() => model.find({ foo: 1 }, { fields: [ 'bar' ] }))
-			.then((documents) => {
-				let document = documents[0];
-				document.data.foo = 2;
-				expect(() => document.save()).to.not.throw(Error);
-			})
-			.then(() => model2.find({ foo: 1 }, { fields: [ 'bar' ] }))
-			.then((documents) => {
-				let document = documents[0];
-				document.data.foo = 2;
-				expect(() => document.save()).to.throw(Error);
-			});
-	});
-
 	it('should return number of matched records in UnimongoModel#count', function() {
 		let model = createModel('testings', { foo: Number });
 
@@ -391,26 +265,6 @@ describe('UnimongoModel', function() {
 			.then((documents) => {
 				expect(documents.length).to.equal(1);
 				expect(documents[0].data.foo).to.equal(2);
-			});
-	});
-
-	it('should handle setting and removing items from the same array in UnimongoDocument#save', function() {
-		let model = createModel('testings', { foo: [ String ] });
-
-		let document = new UnimongoDocument(model, {
-			foo: [ 'a', 'b', 'c' ]
-		});
-
-		return document.save()
-			.then((document) => {
-				document.data.foo = [ 'a', 'c' ];
-
-				return document.save();
-			})
-			.then((document) => {
-				expect(document.data.foo.length).to.equal(2);
-				expect(document.data.foo[0]).to.equal('a');
-				expect(document.data.foo[1]).to.equal('c');
 			});
 	});
 
