97aa1a3edbe066ac2504eeab54a06ab238d6452b
Chris Breneman <crispy@cluenet.org>
2018-10-12 11:07:58 -0400

    Fix update not translating mongo errors

--- test/mongo-error.js
+++ test/mongo-error.js
@@ -59,4 +59,25 @@ describe('MongoError', function() {
 
 		return promise;
 	});
+
+	it('should parse error on duplicated keys in update', async function() {
+		let model = createModel('testings', { foo: String, bar: String });
+		model.index({ foo: 1, bar: 1 }, { unique: true });
+
+		let collection = await model.collectionPromise;
+		await collection.insertOne({ foo: 'one', bar: 'two' });
+		await collection.insertOne({ foo: 'one', bar: 'three' });
+		try {
+			await model.update({ bar: 'three' }, { $set: { bar: 'two' } }, { forceAtomic: true });
+		} catch (err) {
+			expect(err).to.be.an.instanceOf(MongoError);
+			expect(err.code).to.equal(XError.ALREADY_EXISTS);
+			expect(err.data.keys.length).to.equal(2);
+			expect(err.data.keys[0]).to.equal('foo');
+			expect(err.data.keys[1]).to.equal('bar');
+			return;
+		}
+		throw new XError(XError.INTERNAL_ERROR, 'Expected rejection');
+	});
+
 });
